e7348be47f5e2cc789ee94a3e003ba8dff211ecb
simhadri_student <narayanasurada1@gmail.com>
2025-03-27 13:32:41 +0530

    Time: 79 ms (8.2%), Space: 106.3 MB (5.33%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -23,30 +23,24 @@ public:
 
         }
         vector<int> A;
-        for(auto i : nums){
-            if( i == max_first){
+
+        for(int i = 0;i < nums.size();i++){
+            if( nums[i] == max_first){
                     A.push_back(1);
             }
             else{
                 A.push_back(0);
             }
+            if(i!=0)A[i]+=A[i-1];
 
         }
-        for(int i = 1;i < A.size();i++){
-            A[i] += A[i-1];
-        }
+        
         long long int last = A[A.size() - 1];
         for(int i = 0;i < A.size();i++){
            if(check(A[i],i+1) == 0&&check(last - A[i],A.size() - i -1) == 0){
             return i;
            }
         }
-    
-         
-
-
-
-  
        return -1;
     }
 };
