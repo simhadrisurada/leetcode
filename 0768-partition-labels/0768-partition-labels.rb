df60071c0081385dfd0a9af689561e19d2431fe4
simhadri_student <narayanasurada1@gmail.com>
2025-03-30 18:04:14 +0530

    Time: 12 ms (60%), Space: 211.7 MB (15%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.rb
+++ 0768-partition-labels/0768-partition-labels.rb
@@ -3,6 +3,7 @@
 def partition_labels(s)
 hash = Hash.new
 i = 0 
+#capture the start index and the ending index using hashes
 while i < s.length
 if hash[s[i]] == nil
 hash[s[i]] = [i,0] 
@@ -10,12 +11,13 @@ end
 hash[s[i]][1] = i
 i+=1
 end
-
+# convert hashes into arr of from arr[n][2] where n is the number different charecters with their intervals in it 
 nu = []
 hash.each_key do|key|
 nu.push(hash[key])
 end
-nu = nu.sort
+nu = nu.sort # sort them by their starting indexes
+############################ merging the intervals ###########################
 i = 1
 temp = nu[0]
 while i < nu.length
@@ -35,15 +37,16 @@ i-=1
 end
 i+=1
 end
+##############################################################################
+############ capture the answer ##############################################
 i = 0
 nuu = []
 while i < nu.length
 nuu.push(nu[i][1] - nu[i][0] + 1)
 i+=1
 end
+#############################################################################
 
-
-return nuu
-
-    
+return nuu 
+######## return the  answer ###########
 end
