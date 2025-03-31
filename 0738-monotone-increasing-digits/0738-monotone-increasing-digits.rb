7d23c4573a930c85264574cf19ba89ab8ffd870e
simhadri_student <narayanasurada1@gmail.com>
2025-03-31 17:44:59 +0530

    Time: 4 ms (0%), Space: 211.6 MB (0%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.rb
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.rb
@@ -1,12 +1,16 @@
 # @param {Integer} n
 # @return {Integer}
 def monotone_increasing_digits(n)
+########## converting the number to the array format ##################
 nu = []
 while n != 0
 nu.push(n%10)
 n/=10
 end
 nu = nu.reverse
+#######################################################################
+
+################# optmising the number for the given condition ################################
 stack = []
 i = 0
 while i < nu.length
@@ -23,6 +27,11 @@ while i < nu.length
     end
 i+=1
 end
+###########################################################################################################
+
+
+######################################### final touch #######################################################
+### whenever the flag is changed then the all number after that will be moved to 9 for the given condition ####
 flag = 0
 i = 0
 while i < nu.length
@@ -32,5 +41,6 @@ end
 nu[i] = 9 if flag == 1
 i+=1
 end
+################################################################################################################
 return nu.join('').to_i
 end
