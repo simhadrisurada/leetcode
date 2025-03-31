a207b0178641960cbc787cc7729caec14f21c3cc
simhadri_student <narayanasurada1@gmail.com>
2025-03-31 19:27:19 +0530

    Time: 170 ms (17.39%), Space: 230.6 MB (8.7%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.rb
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.rb
@@ -1,30 +1,33 @@
 # @param {Integer[]} heights
 # @return {Integer}
 def largest_rectangle_area(heights)
+######### for creating monotonically increasing stack #############################################################
 stack = []
-hash = []
+hash = [] # ===>>>> for debugging ! <<<============================================================================
 i = 0
 max = 0
 while i < heights.length
-    if stack.length == 0 || stack.last[0] <= heights[i]
+    if stack.length == 0 || stack.last[0] <= heights[i] #===>>>conditions for stack construction or addition <<<===
         stack.push([heights[i],i]) 
 
-    elsif stack.last[0] > heights[i]
-        n = stack.last[1]
-        while stack.length != 0 && stack.last[0] > heights[i]
+    elsif stack.last[0] > heights[i] 
+        n = stack.last[1] # ==>>>> for previous greter element's index <<<<========================================
+        while stack.length != 0 && stack.last[0] > heights[i]#=====>>>> destruction <<<<<==========================
             hash.push(stack.last)
-            n = stack.last[1]
-            if max < (i - stack.last[1])*stack.last[0]
+            n = stack.last[1] # ==>>>> for previous greter element's index <<<<===================================
+            if max < (i - stack.last[1])*stack.last[0] ### >>>> finding max rectangle in destruction <<<< ########
                 max = (i - stack.last[1])*stack.last[0] 
             end
         stack.pop
         end
-        if stack.length == 0 || stack.last[0] < heights[i]
-            stack.push([heights[i],n]) 
+        if stack.length == 0 || stack.last[0] < heights[i] #===>>>conditions for stack construction or addition <<<===
+            stack.push([heights[i],n])
         end
     end
 i+=1
 end
+####################################################################################################################
+###########>>>>>>>>>>>>>>>>>   finding max rectangle in stack that is left <<<<<<<<<<<<<<<<<<<<#####################
 i = 0 
 while i < stack.length
     if max < (heights.length - stack[i][1])*stack[i][0]
@@ -32,8 +35,8 @@ while i < stack.length
     end
 i+=1
 end
+####################################################################################################################
 
-
-return max
+return max ######## taking max to the destination ##################################################################
     
 end
