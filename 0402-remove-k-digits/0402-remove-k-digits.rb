0168ec12b4c7c7a9c9496dc5429e58c60ee830ab
simhadri_student <narayanasurada1@gmail.com>
2025-03-31 15:26:18 +0530

    Time: 424 ms (0%), Space: 324.2 MB (0%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.rb
+++ 0402-remove-k-digits/0402-remove-k-digits.rb
@@ -2,42 +2,50 @@
 # @param {Integer} k
 # @return {String}
 def remove_kdigits(num, k)
-return "0" if num.length == 1 
-o = 0 
+########  converting the given input to my comfort ######################
 num = num.split('')
 i = 0 
 while i < num.length
-num[i] = num[i].to_i
+    num[i] = num[i].to_i
 i+=1
 end
+##########################################################################
+o = 0 #==> flag for deleting ######
+
+################  stack for optimal deletion #############################
 stack = []
 i = 0
 while i < num.length
-if stack.length == 0
-stack.push(num[i])
-elsif stack.last >= num[i]
-while stack.length != 0 && stack.last > num[i] && o < k
-o+=1
-stack.pop
-end
-stack.push(num[i])
-else
-stack.push(num[i])
-end 
+    if stack.length == 0
+        stack.push(num[i])
+    elsif stack.last >= num[i]
+        while stack.length != 0 && stack.last > num[i] && o < k
+            o+=1
+            stack.pop
+        end
+        stack.push(num[i])
+    else
+        stack.push(num[i])
+    end 
 i+=1
 end
+##########################################################################
+
+###################### if still there are digits to delete #############
 while o < k
-stack.pop
+    stack.pop
 o+=1
 end
+########################################################################
 
+################## converting number array to string ####################
 i = 0
 ans = 0
 while i < stack.length
-ans = (ans*10) + stack[i]
+    ans = (ans*10) + stack[i]
 i+=1
 end
 
 return ans.to_s
-
+#########################################################################
 end
