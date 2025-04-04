d43fa08771a2a6baf31f6f41d21159e358decf88
simhadri_student <narayanasurada1@gmail.com>
2025-04-04 22:04:58 +0530

    Time: 0 ms (100%), Space: 212 MB (60%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.rb
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.rb
@@ -9,33 +9,32 @@
 # end
 # @param {TreeNode} root
 # @return {TreeNode}
+############### max depth #############################
 def max_len(tree,len)
-if tree == nil 
-return len
-end
+    if tree == nil 
+        return len
+    end
 n1,n2 = 0,0
 n1 = max_len(tree.left,len+1) 
 n2 = max_len(tree.right,len+1) 
 return [n1,n2].max
 end
+######################################################
+########## for finding max depth traverse ###############
 def dfs(tree,len,target,hash)
-if tree == nil 
-return len
-end
+    if tree == nil 
+        return len
+    end
 n1 = dfs(tree.left,len+1,target,hash)
 n2 = dfs(tree.right,len+1,target,hash)
-hash.push(tree) if n1 == target && n1==n2
+hash.push(tree) if n1 == target && n1==n2 ## for saving last nax depth node #
 return [n1,n2].max
 end
-
-
-
-
+##########################################################
 def lca_deepest_leaves(root)
 hash =  []
 m = max_len(root,0)
 dfs(root,0,m,hash)
-
-return hash[-1]
+return hash[-1] # last is the answer
     
 end
