0f84ad238632d291a303e877a5bda6a38971e5ea
simhadri_student <narayanasurada1@gmail.com>
2025-04-04 19:46:55 +0530

    Time: 1 ms (60%), Space: 212 MB (60%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.rb
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def max_len(tree,len)
+if tree == nil 
+return len
+end
+n1,n2 = 0,0
+n1 = max_len(tree.left,len+1) 
+n2 = max_len(tree.right,len+1) 
+return [n1,n2].max
+end
+def dfs(tree,len,target,hash)
+if tree == nil 
+return len
+end
+n1 = dfs(tree.left,len+1,target,hash)
+n2 = dfs(tree.right,len+1,target,hash)
+hash.push(tree) if n1 == target && n1==n2
+return [n1,n2].max
+end
+
+
+
+
+def lca_deepest_leaves(root)
+hash =  []
+m = max_len(root,0)
+dfs(root,0,m,hash)
+
+return hash[-1]
+    
+end
