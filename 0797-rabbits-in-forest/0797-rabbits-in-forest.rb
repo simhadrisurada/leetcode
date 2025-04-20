c961a5fc95cfdff7e6bd09df012c72ef6b160c60
simhadri_student <narayanasurada1@gmail.com>
2025-04-20 10:43:47 +0530

    Time: 12 ms (0%), Space: 211.7 MB (100%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.rb
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.rb
@@ -1,9 +1,14 @@
 # @param {Integer[]} answers
 # @return {Integer}
 def num_rabbits(answers)
+############################ sort then given info #########################
+######### sorted linearity gives optimal way to solve the problem ########
 answers = answers.sort
-count=+1
+###########################################################################
+count = 1
 ans = 0
+
+################# algorithm for finding exact same color rabbits#############
 i = 1 
 while i < answers.length
 if answers[i] != answers[i-1]
@@ -17,9 +22,14 @@ count+=1
 end
 i+=1
 end
+#############################################################################
+
+################## for an exceptional case ##################################
 n = 0
 n = answers[i-1] + 1 if count%(answers[i-1]+1) != 0
 ans += (((count/(answers[i-1] + 1))*(answers[i-1] + 1)) + n)
 
+#############################################################################
     return ans
+####==>>> here the solution ends |:)| ####################
 end
