70fc2f49c687191663c9acc1248c7824459eca6e
simhadri_student <narayanasurada1@gmail.com>
2025-04-21 08:26:21 +0530

    Time: 906 ms (0%), Space: 232.8 MB (100%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.rb
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.rb
@@ -2,20 +2,15 @@
 # @param {Integer} lower
 # @param {Integer} upper
 # @return {Integer}
+
+
 def check(nu,low,high,pre)
 if pre - nu[0] >= low && pre - nu[0] <=high && pre - nu[-1] >= low && pre - nu[-1] <=high
 return true 
 end
 return false
 end
-def bin_1()
-low = lower
-high = upper
-while low <= high
-out = check(nums,lower,upper,low)
-low += 1
-end
-end
+
 def number_of_arrays(differences, lower, upper)
 nums = differences
 i = nums.length - 1
