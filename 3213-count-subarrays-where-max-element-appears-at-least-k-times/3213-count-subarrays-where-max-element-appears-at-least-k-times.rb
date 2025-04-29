eab9310744e2ecca6643203f06feec28b0146282
simhadri_student <narayanasurada1@gmail.com>
2025-04-29 20:29:10 +0530

    Time: 87 ms (100%), Space: 232.6 MB (0%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.rb
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.rb
@@ -11,27 +11,31 @@ i+=1
 end
 return 0 if nu.length == 0
 i = 0
-puts nu.inspect
 nuu = nu
 nu = nu.reverse
-puts nu.inspect
+## making front array##
 while i < nu.length - 1
 nu[i] = nu[i] - nu[i+1]
 i+=1
 end
+######################
 count = 0
 i = 0
+## making back array###
 while i < nuu.length
 nuu[i] = nums.length - nuu[i] + 1
 i+=1
 end
+#######################
 nu = nu.reverse
 i = 0
 j = k-1
+######## calculating by front and back possibilities #####
 while j < nuu.length
 count+=(nu[i]*nuu[j])
 i+=1
 j+=1
 end
+#########################################################
     return count
 end
