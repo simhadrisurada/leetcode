e43c1dfafdf7b2ee9147186a8db624556da54b8d
simhadri_student <narayanasurada1@gmail.com>
2025-05-09 21:21:25 +0530

    Time: 90 ms (82.35%), Space: 243.1 MB (29.41%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.rb
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.rb
@@ -14,22 +14,17 @@ return root if root == nil
 queue = [root]
     while queue.length != 0
         n = queue.length
-        nu2 = []
         i = 0
+        nu = []
             while i < n
                 nu = queue[0]
                 queue.delete_at(0)
                 queue.push(nu.left) if nu.left != nil
                 queue.push(nu.right) if nu.right != nil
-                nu2.push(nu)
+                nu.next = queue[0]
                 i+=1
             end
-            i = 0
-            while i < n - 1
-                nu2[i].next = nu2[i+1]
-            i+=1
-            end
-            nu2[-1].next = nil
+            nu.next = nil
     end
     return root
 end
