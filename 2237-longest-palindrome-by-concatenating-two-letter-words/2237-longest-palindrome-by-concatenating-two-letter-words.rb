3cd1ef49287c1962b39a22acd60ac663f2422942
simhadri_student <narayanasurada1@gmail.com>
2025-05-25 21:18:42 +0530

    Time: 815 ms (0%), Space: 227.8 MB (0%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.rb
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.rb
@@ -15,14 +15,15 @@ i =0
 c = 0
 flag = 0
 while i < words.length
-if hash[words[i].reverse] != nil && hash[words[i].reverse] != 0 && hash[words[i]] != 0 && words[i] != words[i].reverse
-hash[words[i].reverse] -= 1
+rev  = words[i].reverse
+if hash[rev] != nil && hash[rev] != 0 && hash[words[i]] != 0 && words[i] != rev
+hash[rev] -= 1
 hash[words[i]] -= 1
 c+=4
-elsif hash[words[i]] > 1 && words[i] == words[i].reverse
+elsif hash[words[i]] > 1 && words[i] == rev
 hash[words[i]] -= 2
 c+=4
-elsif hash[words[i]] == 1 && words[i] == words[i].reverse
+elsif hash[words[i]] == 1 && words[i] == rev
 flag = 1
 end
 i+=1
