cc9581c9f75103b79ce042f08787e2cd54d37749
simhadri_student <narayanasurada1@gmail.com>
2025-05-30 20:19:05 +0530

    Time: 15 ms (30.43%), Space: 215.3 MB (7.07%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.rb
@@ -0,0 +1,32 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer[]}
+def top_k_frequent(nums, k)
+hash = Hash.new
+i = 0
+while i < nums.length
+if hash[nums[i]] == nil
+hash[nums[i]] = 1
+else
+hash[nums[i]] += 1
+end
+i+=1
+end
+nu = []
+hash1 = Hash.new
+hash.each_key do|key|
+if hash1[hash[key]] == nil
+hash1[hash[key]] = [key]
+else
+hash1[hash[key]] += [key]
+end
+end
+hash1 = hash1.sort
+ i = hash1.length - 1
+ while i >= 0
+ nu+=hash1[i][1]
+ i-=1
+ end
+ return nu[0,k]
+   
+end
