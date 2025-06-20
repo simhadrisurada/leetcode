26658d9e00a1e232752ee86b4689f056bdbe327a
simhadri_student <narayanasurada1@gmail.com>
2025-06-20 23:26:07 +0530

    Time: 289 ms (0%), Space: 212.3 MB (100%) - LeetHub

--- 2754-maximum-strength-of-a-group/2754-maximum-strength-of-a-group.rb
+++ 2754-maximum-strength-of-a-group/2754-maximum-strength-of-a-group.rb
@@ -1,18 +1,19 @@
 # @param {Integer[]} nums
 # @return {Integer}
-$max = []
+$max = -Float::INFINITY
 def go(nums,i,now,n)
+$max = now if now > $max && n != 0
 if i >= nums.length
-$max.push(now) if n != 0
 return 
 end
 go(nums,i+1,now*nums[i],n+1)
 go(nums,i+1,now,n)
+
 end
 def max_strength(nums)
-go(nums,0,1,0)
 
+go(nums,0,1,0)  
 n = $max
-$max = []
- return n.max  
+$max = -Float::INFINITY
+ return n  
 end
