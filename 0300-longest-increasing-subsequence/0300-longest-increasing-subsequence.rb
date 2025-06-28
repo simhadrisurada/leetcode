53e1be2aba4192fed3772320133aaa8c6c0be0fe
simhadri_student <narayanasurada1@gmail.com>
2025-06-28 21:54:53 +0530

    Time: 1352 ms (52.94%), Space: 236.2 MB (5.88%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.rb
@@ -0,0 +1,26 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def give(nu,now)
+ans = []
+i = 0
+while i < nu.length
+if nu[i][-1] > now && ans.length < nu[i].length
+ans = nu[i]
+end
+i+=1
+end
+return ans
+end
+def length_of_lis(nums)
+return 1 if nums.length == 1
+max = 0
+nu = [[nums[-1]]]
+i  = nums.length - 2
+while i >= 0
+now = give(nu,nums[i])
+nu.push(now+[nums[i]])
+max = now.length + 1 if max < now.length + 1
+i-=1
+end
+   max 
+end
