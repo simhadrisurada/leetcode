011854e0f93341e711ac2381681768dbd06b247a
simhadri_student <narayanasurada1@gmail.com>
2025-07-20 23:28:43 +0530

    Time: 335 ms (100%), Space: 216.3 MB (100%) - LeetHub

--- 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.rb
+++ 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.rb
@@ -17,7 +17,7 @@ while low <= high
 mid = (low + high)/2
 plot1 = give(nums,cost,mid) 
 plot2 = give(nums,cost,mid + 1)
-puts [plot1,plot2,ans].inspect
+
 ans = [plot1,plot2,ans].min
     if plot1 > plot2
         low = mid + 1
